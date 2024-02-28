be785c0f7e1cbad7c09687648b36e63b0c9ee912
Krishna Gupta <121439479+krishnagptamcs@users.noreply.github.com>
2024-02-28 22:24:19 +0530

    card section done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import HeroSection from "@/components/HeroSection";
 import WhyChooseUs from "@/components/WhyChooseUs";
+import TestimonialCards from "@/components/ui/TestimonialCards";
 
 export default function Home() {
   return (
     <>
-     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-
-      <HeroSection/>
-      <WhyChooseUs/>
-     </main>
+      <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+        <HeroSection />
+        <WhyChooseUs />
+        <TestimonialCards />
+      </main>
     </>
   );
 }
